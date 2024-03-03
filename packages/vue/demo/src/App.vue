2ba5305ede235612ca7de156a5ee0ef02248bf5c
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2024-03-03 21:14:22 +0100

    add demo folder on vue

---
+++ packages/vue/demo/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useCollapse } from '@compotes/vue'
+import { shallowRef } from 'vue'
+
+const collapseEl = shallowRef<null | HTMLElement>(null)
+useCollapse(collapseEl)
+</script>
+
+<template>
+  <button class="c-collapse-trigger" aria-controls="my-collapse">
+    Trigger collapse
+  </button>
+  <div id="my-collapse" ref="collapseEl" class="c-collapse">
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Mollitia facere possimus impedit facilis culpa illo earum deserunt consequuntur minus.
+    </p>
+  </div>
+</template>
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,8 +1,8 @@
-import { useDrag } from '@/composables/drag'
-import { useDrilldown } from '@/composables/drilldown'
-import { useDropdown } from '@/composables/dropdown'
-import { useMarquee } from '@/composables/marquee'
-import { useCollapse } from '@/composables/collapse'
+import { useDrag } from './composables/drag'
+import { useDrilldown } from './composables/drilldown'
+import { useDropdown } from './composables/dropdown'
+import { useMarquee } from './composables/marquee'
+import { useCollapse } from './composables/collapse'
 
 export {
   useCollapse,
