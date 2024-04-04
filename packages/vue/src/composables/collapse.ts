f153d66c1c4f8601d0dcc0c6c5a7c6ff89844a57
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2024-04-04 19:40:03 +0200

    update package

--- packages/vue/src/composables/collapse.ts
+++ packages/vue/src/composables/collapse.ts
@@ -1,6 +1,6 @@
 import type { CollapseOptions } from 'compotes'
 import { Collapse } from 'compotes'
-import { type Ref, computed } from 'vue'
+import type { Ref } from 'vue'
 import { useParent } from './_parent'
 
 export function useCollapse(
@@ -8,9 +8,4 @@ export function useCollapse(
   options?: CollapseOptions,
 ) {
   return useParent(Collapse, el, options)
-  // const collapse = useParent(Collapse, el, options)
-  // return {
-  //   collapse,
-  //   isExpanded: computed(() => collapse.value?.isExpanded),
-  // }
 }
